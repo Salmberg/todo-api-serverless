d86938116bd49b8cd2af1e3f39ae8580fb6c9c90
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-11-25 17:18:01 +0100

    Now i can crate a Todo with all the parameters

--- functions/postTodo/index.js
+++ functions/postTodo/index.js
@@ -1,20 +1,43 @@
 const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
 const { sendResponse } = require('../../responses/index');
 const db = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
-    const todo = JSON.parse(event.body);
+  try {
+    const requestBody = JSON.parse(event.body);
 
-   try {
-   await db.put({
-        TableName: 'todos-db',
-        Item: todo
+    // Kolla att alla oblikatoriska fält finns 
+    if (!requestBody.title || !requestBody.text) {
+      return sendResponse(400, { success: false, message: 'Title and text are required fields.' });
+    }
+
+    // Skapa en timestamp för createdAt och modifiedAt
+    const timestamp = new Date().toISOString();
+
+    // Skapa en Todo-objekt med alla fält
+    const todo = {
+      id: generateId(),
+      title: requestBody.title,
+      text: requestBody.text,
+      createdAt: timestamp,
+      modifiedAt: timestamp,
+    };
+
+    await db.put({
+      TableName: 'todos-db',
+      Item: todo,
     }).promise();
 
-    return sendResponse(200, {success: true, todo});
-   } catch (error) {
+    return sendResponse(201, { success: true, todo });
 
-    return sendResponse(500, {success: false, message: "Something went wrong when posting a Todo", error});
-   }
+  } catch (error) {
+    console.error('Error creating Todo:', error);
+    return sendResponse(500, { success: false, message: 'Internal Server Error' });
+  }
+};
 
-}
+// Funktion som genererar ett unikt ID
+function generateId() {
+    return uuidv4();
+  }
