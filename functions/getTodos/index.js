91d0b3f0c56fff869fe8ec97e428b4ae523dc977
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-11-27 18:53:21 +0100

    Now done with the auth middleware

--- functions/getTodos/index.js
+++ functions/getTodos/index.js
@@ -1,16 +1,29 @@
 const AWS = require('aws-sdk');
 const { sendResponse } = require('../../responses/index');
+const { validateToken } = require('../middleware/auth');
+const middy = require('@middy/core');
 const db = new AWS.DynamoDB.DocumentClient();
 
 
 
-exports.handler = async (event, context) => {
+const getTodos = async (event, context) => {
+
+    if (event?.error && event?.error === '401')
+        return sendResponse(401, {success: false, message: 'Invalid Token'});
 
     const {Items} = await db.scan({
         TableName: 'todos-db',
+        
     }).promise();
 
         return sendResponse(200, {success: true, todos : Items});
    
 
 }
+
+const handler = middy(getTodos)
+    .use(validateToken)
+
+
+
+module.exports = {handler};
---
+++ functions/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const validateToken = {
+    before: async (request) => {
+
+        try {
+            const token = request.event.headers.authorization.replace('Bearer ', '');
+
+            if (!token) throw new Error('No token provided');
+
+            const data = jwt.verify(token, 'aabbcc');
+
+            request.event.id = data.id;
+            request.event.username = data.username;
+
+            return request.response;
+        } catch (error) {
+            request.event.error = '401', { message: 'Unauthorized' };
+            return request.response;
+        }
+    }
+}  
+
+module.exports = { validateToken };
